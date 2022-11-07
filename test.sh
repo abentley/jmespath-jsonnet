eb0741ffe96c614e64f04dcf3a4f636196fd8579
Aaron Bentley <aaron@aaronbentley.com>
2022-11-06 20:27:08 -0500

    Test exits nonzero on error.

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 test_basename=${1-*}
 test_files=($(find tests -name "$test_basename.jsonnet"))
+error=false
 for test_file in ${test_files[@]}; do
     if [ $# -gt 1 ]; then
         tests=($2)
@@ -12,8 +13,12 @@ for test_file in ${test_files[@]}; do
     for test in ${tests[@]}; do
         CMD="jsonnet -SJ src -J test $args --tla-str testName=$test $test_file"
         if ! ($CMD 2> /dev/null); then
+            error=true
             echo $test: ERROR
             $CMD
         fi
     done
 done
+if $error; then
+    exit 1
+fi
