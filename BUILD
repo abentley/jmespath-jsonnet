c2ffefb1ccd367877b501df8ceec4688aa34bf56
Aaron Bentley <aaron@aaronbentley.com>
2022-11-16 18:35:20 -0500

    Test with Bazel

---
+++ BUILD
@@ -0,0 +1,8 @@
+sh_test(
+    name = "test",
+    srcs = ["test.sh"],
+    data = glob(["tests/*"]) + [
+        'src/tokens.libsonnet',
+        'src/jmespath.libsonnet',
+    ],
+)
--- test.sh
+++ test.sh
@@ -13,10 +13,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+set -eu
 
 test_basename=${1-*}
 test_files=($(find tests -name "$test_basename.jsonnet"))
 error=false
+if [ "${#test_files[@]}" == "0" ]; then
+    echo No test files found
+    exit 1
+fi
 for test_file in ${test_files[@]}; do
     if [ $# -gt 1 ]; then
         tests=($2)
@@ -26,7 +31,7 @@ for test_file in ${test_files[@]}; do
     if [ $test_file != ${test_files[0]} ]; then echo; fi
     echo $test_file
     for test in ${tests[@]}; do
-        CMD="jsonnet -SJ src --max-trace 1000 -J test $args --tla-str testName=$test $test_file"
+        CMD="jsonnet -SJ src --max-trace 1000 -J test --tla-str testName=$test $test_file"
         if ! ($CMD 2> /dev/null); then
             error=true
             echo $test: ERROR
