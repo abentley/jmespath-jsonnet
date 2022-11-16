16ec96b980cabbb2e39818423b5755f9bdb81a12
Aaron Bentley <aaron@aaronbentley.com>
2022-11-16 12:48:37 -0500

    Cleanup

--- compliance.sh
+++ compliance.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export JSONNET_PATH=$(dirname $(realpath $0))/src
-jsonnet --tla-code-file json=/proc/self/fd/0 --tla-str jmespath="$1"\
-    --exec \
+jsonnet --tla-code-file json=/proc/self/fd/0 --tla-str jmespath="$1" --exec \
     "local lib = import 'jmespath.libsonnet';
     function(json, jmespath) lib.search(jmespath, json)"
