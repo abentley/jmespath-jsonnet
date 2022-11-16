e3462ab5e36ba864483770346aaa4784d1bd4473
Aaron Bentley <aaron@aaronbentley.com>
2022-11-16 08:50:27 -0500

    Auto-determine jsonnet path.

--- compliance.sh
+++ compliance.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-jsonnet -J /home/abentley/hacking/jmespath-jsonnet/src --tla-code-file json=/proc/self/fd/0 --tla-str jmespath="$1"\
+export JSONNET_PATH=$(dirname $(realpath $0))/src
+jsonnet --tla-code-file json=/proc/self/fd/0 --tla-str jmespath="$1"\
     --exec \
     "local lib = import 'jmespath.libsonnet';
     function(json, jmespath) lib.search(jmespath, json)"
